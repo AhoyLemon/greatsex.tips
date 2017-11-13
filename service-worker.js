5eb78fb3c328f916df90068dd9d9adfed86ae111
Lemon <AhoyLemon@users.noreply.github.com>
2017-11-13 11:42:26 -0600

    First pass at service worker.
    Relates to #7

---
+++ service-worker.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const cacheName = 'v0.1a';
+
+self.addEventListener('install', e => {
+  // once the SW is installed, go ahead and fetch the resources
+  // to make this work offline
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll([
+        '/index.html',
+        '/manifest.json',
+        '/css/tips.css',
+        '/js/min/tips.min.js',
+        '/js/min/vue.min.js',
+        '/img/bg-offline.jpg',
+        '/offline.html'
+      ]).then(() => self.skipWaiting());
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
