b22384080f832597271809c7b2aaa901bcb4e452
SanguinaryNovel <SanguinaryNovel@users.noreply.github.com>
2017-11-16 23:50:26 -0500

    Works, fixing spacing errors

--- js/partials/tips/_danceForHim.js
+++ js/partials/tips/_danceForHim.js
@@ -1,23 +1,21 @@
 var danceForHim = [
   [
-    "Show him your sultry moves to get in the mood:",
+    "Show him your sultry moves:",
     "Dance moves to make your man melt:",
-    "A seductive gift for special occasions:",
-    "Play the powerful seductress:"
   ],
-    "Play ",
+    " Play ",
   [
     "<i>The Breakfast Machine</i>",
     "Gregorian chant",
     "the screaming of the damned"
   ],
-    " in the background while slowly stripping out of ",
+    " while seductively stripping out of ",
   [
     "your <i>Home Depot</i> uniform",
     "a live hive of African wasps",
     "your meat dress"
   ],
-  ".",
+  ". ",
     "Get intimate at the end by ",
   [
     "jiggling",
@@ -33,8 +31,5 @@ var danceForHim = [
     "rubbing your butt in his face.",
     "climbing into his tshirt.",
     "slithering like a snake on the floor."
-  ],
-  [
-    "What a tease!"
   ]
 ];
